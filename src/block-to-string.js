50ac3fb57699587a898ae3361a266bfc3d6c9f3f
Sergei Orlov <hello@orlow.dev>
2021-05-17 16:35:32 +0300

    :sparkles: Add support for annotations in the page title (color, code, underline, etc.)

---
+++ src/block-to-string.js
@@ -0,0 +1,70 @@
+const { pipeExtend } = require("or-pipets")
+
+const pick = (key) => (obj) => obj[key]
+
+const ifTrue = (predicate, transformer, orElse) => (data) =>
+	predicate(data) ? transformer(data) : orElse(data)
+
+const id = (x) => x
+
+const annotateBold = ifTrue(pick("bold"), ({ content }) => ({ content: `**${content}**` }), id)
+const annotateItalic = ifTrue(pick("italic"), ({ content }) => ({ content: `_${content}_` }), id)
+const annotateCode = ifTrue(pick("code"), ({ content }) => ({ content: `\`${content}\`` }), id)
+const annotateStrikethrough = ifTrue(
+	pick("strikethrough"),
+	({ content }) => ({ content: `~~${content}~~` }),
+	id,
+)
+const annotateUnderline = ifTrue(
+	pick("underline"),
+	({ content }) => ({ content: `<u>${content}</u>` }),
+	id,
+)
+const annotateColor = ifTrue(
+	({ color }) => color != "default",
+	({ content, color }) => ({ content: `<span notion-color="${color}">${content}</span>` }),
+	id,
+)
+const annotateLink = ifTrue(
+	pick("link"),
+	({ content, link }) => ({ content: `[${content}](${link.url ? link.url : link})` }),
+	id,
+)
+
+const stylize = pipeExtend(annotateBold)
+	.pipeExtend(annotateItalic)
+	.pipeExtend(annotateCode)
+	.pipeExtend(annotateStrikethrough)
+	.pipeExtend(annotateUnderline)
+	.pipeExtend(annotateColor)
+	.pipeExtend(annotateLink)
+
+exports.blockToString = (textBlocks) =>
+	textBlocks.reduce((text, textBlock) => {
+		const data = {
+			...textBlock.text,
+			...textBlock.annotations,
+		}
+
+		if (textBlock.type == "mention") {
+			if (textBlock.mention.type == "user") {
+				data.content = textBlock.plain_text
+			}
+
+			if (textBlock.mention.type == "date") {
+				if (textBlock.mention.date.end) {
+					data.content = `${textBlock.mention.date.start} → ${textBlock.mention.date.start}`
+				} else {
+					data.content = textBlock.mention.date.start
+				}
+
+				data.content = `<time datetime="${data.content}">${data.content}</time>`
+			}
+
+			if (textBlock.mention.type == "page") {
+				data.content = textBlock.plain_text
+			}
+		}
+
+		return text.concat(stylize.process(data).content)
+	}, "")
--- src/transformers/get-page-title.js
+++ src/transformers/get-page-title.js
@@ -1,33 +1,9 @@
+const { blockToString } = require("../block-to-string")
+
 exports.getNotionPageTitle = (page) => {
 	const titleProperty = Object.keys(page.properties).find(
 		(key) => page.properties[key].type == "title",
 	)
 
-	return page.properties[titleProperty].title
-		.reduce((acc, chunk) => {
-			if (chunk.type == "text") {
-				return acc.concat(chunk.plain_text)
-			}
-
-			if (chunk.type == "mention") {
-				if (chunk.mention.type == "user") {
-					return acc.concat(chunk.plain_text)
-				}
-
-				if (chunk.mention.type == "date") {
-					if (chunk.mention.date.end) {
-						return acc.concat(`${chunk.mention.date.start} → ${chunk.mention.date.start}`)
-					}
-
-					return acc.concat(chunk.mention.date.start)
-				}
-
-				if (chunk.mention.type == "page") {
-					return acc.concat(chunk.plain_text)
-				}
-			}
-
-			return acc
-		}, "")
-		.trim()
+	return blockToString(page.properties[titleProperty].title)
 }
