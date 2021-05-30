32e7728ea7a438f8098b564eec12e3282ed25d21
Sergei Orlov <hello@orlow.dev>
2021-05-30 14:20:56 +0300

    :sparkles: Add support for image transformation in props
    :sparkles: Add support for lowering title level
    :sparkles: Add option for transforming Notion page props to frontmatter
    :heavy_plus_sign: Add gatsby-source-filesystem as a peer dependency
    :bug: Fix indentation of code snippets written in Markdown syntax
    :bug: Fix displaying Markdown tables

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,59 +1,65 @@
 const { getPages } = require("./src/notion-api/get-pages")
-const { getNotionPageMD } = require("./src/transformers/get-page-md")
+const { notionBlockToMarkdown } = require("./src/transformers/notion-block-to-markdown")
 const { getNotionPageProperties } = require("./src/transformers/get-page-properties")
 const { getNotionPageTitle } = require("./src/transformers/get-page-title")
 const YAML = require("yaml")
+const { createRemoteFileNode } = require("gatsby-source-filesystem")
 
-const NODE_TYPE = "Notion"
+const NOTION_NODE_TYPE = "Notion"
+
+exports.onCreateNode = async ({ node, actions: { createNode }, createNodeId, getCache }) => {
+	if (node.internal.type == NOTION_NODE_TYPE) {
+		const filesPropertyKey = Object.keys(node.properties).find(
+			(key) => node.properties[key].type == "files",
+		)
+
+		if (filesPropertyKey) {
+			for (let i = 0; i < node.properties[filesPropertyKey].value.length; i++) {
+				const name = node.properties[filesPropertyKey].value[i].name
+
+				if (name.startsWith("http")) {
+					const fileNode = await createRemoteFileNode({
+						url: name,
+						parentNodeId: node.id,
+						createNode,
+						createNodeId,
+						getCache,
+					})
+
+					if (fileNode) {
+						node.properties[filesPropertyKey].value[i].remoteImage___NODE = fileNode.id
+					}
+				}
+			}
+		}
+	}
+}
 
 exports.sourceNodes = async (
 	{ actions, createContentDigest, createNodeId, reporter },
-	pluginOptions,
+	{ token, databaseId, propsToFrontmatter = true, lowerTitleLevel = true },
 ) => {
-	const pages = await getPages(
-		{ token: pluginOptions.token, databaseId: pluginOptions.databaseId },
-		reporter,
-	)
-
-	/*
-		if (page.properties[key].type == "rich_text") {
-			page.properties[key].rich_text = blockToString(page.properties[key].rich_text)
-		} 
-		*/
+	const pages = await getPages({ token, databaseId }, reporter)
 
 	pages.forEach((page) => {
 		const title = getNotionPageTitle(page)
 		const properties = getNotionPageProperties(page)
-		const frontmatter = Object.keys(properties).reduce(
-			(acc, key) => {
-				if (properties[key].type == "date") {
-					return {
-						...acc,
-						[key]: properties[key].value.start,
-					}
-				}
+		let markdown = notionBlockToMarkdown(page, lowerTitleLevel)
 
-				if (properties[key].type == "rich_text") {
-					return {
-						...acc,
-						[key]: blockToString(properties[key].value),
-					}
-				}
-
-				return {
+		if (propsToFrontmatter) {
+			const frontmatter = Object.keys(properties).reduce(
+				(acc, key) => ({
 					...acc,
-					[key]: properties[key].value,
-				}
-			},
-			{ title },
-		)
-		const markdown = "---\n"
-			.concat(YAML.stringify(frontmatter))
-			.concat("\n---\n\n")
-			.concat(getNotionPageMD(page))
+					[key]: properties[key].value.remoteImage || properties[key].value,
+				}),
+				{ title },
+			)
+
+			markdown = "---\n".concat(YAML.stringify(frontmatter)).concat("\n---\n\n").concat(markdown)
+		}
 
 		actions.createNode({
-			id: createNodeId(`${NODE_TYPE}-${page.id}`),
+			id: createNodeId(`${NOTION_NODE_TYPE}-${page.id}`),
 			title,
 			properties,
 			archived: page.archived,
@@ -65,7 +71,7 @@ exports.sourceNodes = async (
 			parent: null,
 			children: [],
 			internal: {
-				type: NODE_TYPE,
+				type: NOTION_NODE_TYPE,
 				mediaType: "text/markdown",
 				content: markdown,
 				contentDigest: createContentDigest(page),
---
+++ src/transformers/notion-block-to-markdown.js
@@ -0,0 +1,84 @@
+const { blockToString } = require("../block-to-string")
+
+const EOL_MD = "\n"
+
+exports.notionBlockToMarkdown = (block, lowerTitleLevel, depth = 0) =>
+	block.children.reduce((acc, childBlock) => {
+		let childBlocksString = ""
+
+		if (childBlock.has_children) {
+			childBlocksString = "  "
+				.repeat(depth)
+				.concat(childBlocksString)
+				.concat(this.notionBlockToMarkdown(childBlock, lowerTitleLevel, depth + 2))
+				.concat(EOL_MD)
+		}
+
+		if (childBlock.type == "paragraph") {
+			const p = blockToString(childBlock.paragraph.text)
+
+			const isTableRow = p.startsWith("|") && p.endsWith("|")
+
+			const isCodeSnippetLine = block.paragraph?.text[0]?.plain_text?.startsWith("```")
+
+			return acc
+				.concat(p)
+				.concat(isTableRow || isCodeSnippetLine ? EOL_MD : EOL_MD.concat(EOL_MD))
+				.concat(childBlocksString)
+		}
+
+		if (childBlock.type.startsWith("heading_")) {
+			const headingLevel = Number(childBlock.type.split("_")[1])
+
+			return acc
+				.concat(EOL_MD)
+				.concat(lowerTitleLevel ? "#" : "")
+				.concat("#".repeat(headingLevel))
+				.concat(" ")
+				.concat(blockToString(childBlock[childBlock.type].text))
+				.concat(EOL_MD)
+				.concat(childBlocksString)
+		}
+
+		if (childBlock.type == "to_do") {
+			return acc
+				.concat(`- [${childBlock.to_do.checked ? "x" : " "}] `)
+				.concat(blockToString(childBlock.to_do.text))
+				.concat(EOL_MD)
+				.concat(childBlocksString)
+		}
+
+		if (childBlock.type == "bulleted_list_item") {
+			return acc
+				.concat("* ")
+				.concat(blockToString(childBlock.bulleted_list_item.text))
+				.concat(EOL_MD)
+				.concat(childBlocksString)
+		}
+
+		if (childBlock.type == "numbered_list_item") {
+			return acc
+				.concat("1. ")
+				.concat(blockToString(childBlock.numbered_list_item.text))
+				.concat(EOL_MD)
+				.concat(childBlocksString)
+		}
+
+		if (childBlock.type == "toggle") {
+			return acc
+				.concat("<details><summary>")
+				.concat(blockToString(childBlock.toggle.text))
+				.concat("</summary>")
+				.concat(childBlocksString)
+				.concat("</details>")
+		}
+
+		if (childBlock.type == "unsupported") {
+			return acc
+				.concat(`<!-- This block is not supported by Notion API yet. -->`)
+				.concat(EOL_MD)
+				.concat(childBlocksString)
+		}
+
+		return acc
+	}, "")
