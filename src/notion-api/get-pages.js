98fd09e824959e9bc3b78fb5b95bb9d81e96324e
Sergei Orlov <hello@orlow.dev>
2021-06-07 18:15:05 +0300

    :sparkles: Add support for recursive DB fetching
    Should work for cases when a database has over 100 rows
    
    Closes #4

--- src/notion-api/get-pages.js
+++ src/notion-api/get-pages.js
@@ -3,25 +3,44 @@ const { errorMessage } = require("../error-message")
 const { getBlocks } = require("./get-blocks")
 
 exports.getPages = async ({ token, databaseId, notionVersion = "2021-05-13" }, reporter) => {
-	try {
-		const db = await fetch(`https://api.notion.com/v1/databases/${databaseId}/query`, {
-			method: "POST",
-			body: JSON.stringify({
-				page_size: 100,
-			}),
-			headers: {
-				"Content-Type": "application/json",
-				"Notion-Version": notionVersion,
-				Authorization: `Bearer ${token}`,
-			},
-		}).then((res) => res.json())
+	let hasMore = true
+	let startCursor = ""
+	const url = `https://api.notion.com/v1/databases/${databaseId}/query`
+	const body = {
+		page_size: 100,
+	}
 
-		for (let page of db.results) {
-			page.children = await getBlocks({ id: page.id, token, notionVersion }, reporter)
+	const pages = []
+
+	while (hasMore) {
+		if (startCursor) {
+			body.start_cursor = startCursor
 		}
 
-		return db.results
-	} catch (e) {
-		reporter.panic(errorMessage)
+		try {
+			const result = await fetch(url, {
+				method: "POST",
+				body: JSON.stringify(body),
+				headers: {
+					"Content-Type": "application/json",
+					"Notion-Version": notionVersion,
+					Authorization: `Bearer ${token}`,
+				},
+			}).then((res) => res.json())
+			console.log(result.next_cursor, result.results.length)
+
+			startCursor = result.next_cursor
+			hasMore = result.has_more
+
+			for (let page of result.results) {
+				page.children = await getBlocks({ id: page.id, token, notionVersion }, reporter)
+
+				pages.push(page)
+			}
+		} catch (e) {
+			reporter.panic(errorMessage)
+		}
+
+		return pages
 	}
 }
